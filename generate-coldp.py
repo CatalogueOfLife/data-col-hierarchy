4f0d4c26bb8d7eeccbfeb9623aafdcf934eadb60
Markus Döring <m.doering@mac.com>
2022-01-12 12:12:35 +0100

    Use coldp:NameUsage, add remarks and update metadata

--- generate-coldp.py
+++ generate-coldp.py
@@ -7,10 +7,11 @@ from collections import namedtuple
 sheetName = 'CoLH Version 1.4'
 firstRow  = 2
 firstCol  = 'A'
+notesCol  = 'P'
 ranks     = ['superkingdom','kingdom','subkingdom','infrakingdom', 'superphylum','phylum','subphylum','infraphylum', 'superclass','class','subclass','infraclass', 'cohort', 'superorder', 'order']
 
 
-Taxon = namedtuple('Taxon', 'id col name')
+Taxon = namedtuple('Taxon', 'id col name notes')
 synMatcher = re.compile('^(.+) *\[= *(.+) *] *')
 
 parents = []    
@@ -18,20 +19,17 @@ parents = []
 def read(row):
     for col in range(1, len(ranks)+1):
         val = sheet[chr(ord(firstCol) + col - 1) + str(row+1)].value
+        note = sheet[notesCol + str(row+1)].value
         if val:
-            return Taxon(str(row), col, val)
+            return Taxon(str(row), col, val, note)
     return None
 
-def writeName(out, t):
-    out.write("%s,%s,\"%s\"\n" % (t.id, ranks[t.col-1], t.name.replace('"', '""')))
+def writeUsage(out, t, parentID, status):
+    out.write("%s,%s,%s,%s,\"%s\",\"%s\"\n" % (t.id, parentID, status, ranks[t.col-1], t.name.replace('"', '""'), (t.notes or "").replace('"', '""')))
 
 wb = load_workbook(filename = 'CoLH.xlsx')
-with open('name.csv', 'w', newline='') as nout:
-    with open('taxon.csv', 'w', newline='') as tout:
-        with open('synonym.csv', 'w', newline='') as sout:
-            nout.write("ID,rank,scientificName\n")
-            tout.write("ID,parentID,nameID\n")
-            sout.write("nameID,taxonID,status\n")
+with open('NameUsage.csv', 'w', newline='') as out:
+            out.write("ID,parentID,status,rank,scientificName,remarks\n")
             sheet = wb[sheetName]
             row = 1 
             t = read(row)
@@ -41,12 +39,10 @@ with open('name.csv', 'w', newline='') as nout:
                 pid = parents[-1].id if parents else None
                 m = synMatcher.search(t.name)
                 if m:
-                    s = Taxon('s'+str(row), t.col, m.group(2))
-                    t = Taxon(t.id, t.col, m.group(1))
-                    writeName(nout, s)
-                    sout.write("%s,%s,%s\n" % (s.id, t.id, 'synonym'))
-                writeName(nout, t)
-                tout.write("%s,%s,%s\n" % (t.id, pid or '', t.id))
+                    s = Taxon('s'+str(row), t.col, m.group(2), None)
+                    t = Taxon(t.id, t.col, m.group(1), t.notes)
+                    writeUsage(out, s, t.id, 'synonym')
+                writeUsage(out, t, pid or '', 'accepted')
                 row = row + 1
                 parents.append(t)
                 t = read(row)
